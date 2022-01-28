6177e561a145ba9be4fb99809f407d4332c24caa
Ric Evans <emejqz@gmail.com>
2022-01-27 18:57:36 -0600

    override pytest's event loop fixture

--- integrate_tests/test_nats.py
+++ integrate_tests/test_nats.py
@@ -1,7 +1,9 @@
 """Run integration tests for NATS backend."""
 
+import asyncio
 import logging
 
+import pytest
 from mqclient.abstract_backend_tests import integrate_backend_interface, integrate_queue
 from mqclient.abstract_backend_tests.utils import (  # pytest.fixture # noqa: F401 # pylint: disable=W0611
     queue_name,
@@ -10,6 +12,13 @@ from mqclient_nats.nats import Backend
 
 logging.getLogger().setLevel(logging.DEBUG)
 logging.getLogger("flake8").setLevel(logging.WARNING)
+
+
+@pytest.fixture(scope="module")
+def event_loop():  # type: ignore[no-untyped-def]
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 
 class TestNATSQueue(integrate_queue.PubSubQueue):
